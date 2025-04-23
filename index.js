46175c2bfe69075f795ed56ccd44598dca3bdf7a
FunctionJoey <functionjoey@gmail.com>
2025-04-23 16:36:06 -0600

    Variables

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 // window.alert("This is an alert");
 // window.alert("I like pizza!");
 
-document.getElementById("myH1").textContent = "Hello";
-document.getElementById("myP").textContent = "I like pizza!";
+// document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myP").textContent = "I like pizza!";
 
 // This is a comment
 
@@ -13,3 +13,47 @@ document.getElementById("myP").textContent = "I like pizza!";
  This is a comment
  Whoa buddy
 */
+
+/*
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+let firstName = "Bro";
+let favoriteFood = "pizza";
+let email = "example@email.com";
+
+let online = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(`Enrolled: ${isStudent}`);
+
+console.log(`Is this car for sale: ${forSale}`);
+
+console.log(typeof online);
+console.log(`Bro is online: ${online}`);
+
+console.log(age);
+console.log(price);
+console.log(gpa);
+
+console.log(typeof age);
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favoriteFood}`);
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+console.log(`Your email is ${email}`);
+*/
+
+let fullName = "Big Man";
+let age = 34;
+let student = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
+
