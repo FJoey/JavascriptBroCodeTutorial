254cd447361ced64119be18880ffa4c779d33e77
FunctionJoey <functionjoey@gmail.com>
2025-06-02 16:33:11 -0600

    Type conversion

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
+/* -----------------------------------TYPE CONVERSION
+*/
+let x = "0";
+let y = "0";
+let z = "0";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+/*
+let age = window.prompt("How old are you?");
+age = Number(age);
+age += 1;
+
+console.log(age, typeof age);
+*/
+
+
+/* -----------------------------------USER INPUT
+// Window prompt (Easy way)
+let username1;
+username = window.prompt("What's your username?");
+console.log(username);
+
+
+// Textbox ('Professional Way')
+let username2;
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`
+}
+*/
+
+/* -----------------------------------ARITHMETIC
+
+// let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2; // <-exponent
+// students = students % 2;
+// let extraStudents = students % 3;
+
+// augmented assignment operators
+// students += 3;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+
+
+// console.log(students);
+
+/* -----------------------------------VARIABLES
+
+/*
+let fullName = "Big Man";
+let age = 34;
+let student = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
+*/
+
 // console.log("Hello");
 // console.log("I like pizza");
 
@@ -48,53 +124,3 @@ console.log(`The price is $${price}`);
 console.log(`Your gpa is: ${gpa}`);
 console.log(`Your email is ${email}`);
 */
-
-/*
-let fullName = "Big Man";
-let age = 34;
-let student = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `You are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ${student}`;
-*/
-
-// let students = 30;
-
-// students = students + 1;
-// students = students - 1;
-// students = students * 2;
-// students = students / 2;
-// students = students ** 2; // <-exponent
-// students = students % 2;
-// let extraStudents = students % 3;
-
-// augmented assignment operators
-// students += 3;
-// students -= 1;
-// students *= 2;
-// students /= 2;
-// students **= 2;
-// students %= 2;
-
-// students++;
-// students--;
-
-
-// console.log(students);
-
-// User Input
-
-// Window prompt
-
-let username1;
-username = window.prompt("What's your username?");
-console.log(username);
-
-
-// Textbox
-let username2;
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Hello ${username}`
-}
