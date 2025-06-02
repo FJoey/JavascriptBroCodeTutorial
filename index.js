76d9b68788fd86802f4accb91b24f7cefb682fa8
FunctionJoey <functionjoey@gmail.com>
2025-06-02 16:46:05 -0600

    Constants

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-/* -----------------------------------TYPE CONVERSION
+/* -----------------------------------CONSTANTS
+// const = a variable that can't be changed
+
 */
+
+// Constants are normally titled in all-caps, unless they are reference types, like Strings
+const PI = 3.14159;
+let radius;
+let circumference;
+
+// We wouldn't want this to happen for pi (variable reassignment)
+//pi = 420.69;
+
+//radius = window.prompt('Enter the radius of a circle');
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+//console.log(circumference);
+
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + "cm";
+};
+
+/* -----------------------------------TYPE CONVERSION
+
 let x = "0";
 let y = "0";
 let z = "0";
@@ -18,8 +45,8 @@ age = Number(age);
 age += 1;
 
 console.log(age, typeof age);
-*/
 
+*/
 
 /* -----------------------------------USER INPUT
 // Window prompt (Easy way)
