5ad818e18a1b4a112214552849ee290cccb406d6
FunctionJoey <functionjoey@gmail.com>
2025-06-03 15:51:03 -0600

    Math Object

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-/* -----------------------------------COUNTER PROGRAM
+/* -----------------------------------MATH OBJECT
 */
+// Math: built-in object that provides a collection of properties and methods for doing math
+//console.log(Math.PI);
+//console.log(Math.E);
+let x = 3.21;
+let y = 2;
+let z;
+
+//z = Math.round(x);    //Round
+//z = Math.floor(x);    //Round down
+//z = Math.ceil(x);     //Round up
+//z = Math.trunc(x);    //Remove decimal values
+//z = Math.pow(x, y);   //Exponent, x^y
+//z = Math.sqrt(x);     //Square root
+//z = Math.log(x);      //Logarithm
+//z = Math.sin(x);      //Sine
+//z = Math.cos(x);      //Cosine
+//z = Math.tan(x);      //Tangent
+//z = Math.abs(x);      //Absolute value
+//z = Math.sign(x);     //Find sign (+ or -) pos = 1; neg = 0
+//z = Math.max(x, y);   //Find max
+//z = Math.min(x, y);   //Find min
+
+console.log(z);
+
+/* -----------------------------------COUNTER PROGRAM
+
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
@@ -20,7 +46,7 @@ resetBtn.onclick = function(){
     count = 0;
     countLabel.textContent = count;
 }
-
+*/
 /* -----------------------------------CONSTANTS
 // const = a variable that can't be changed
 
