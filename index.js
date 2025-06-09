579a912a5f3a5ceb8a1513c5c6333eb1deb4c8aa
FunctionJoey <functionjoey@gmail.com>
2025-06-09 17:23:14 -0600

    if statements

--- index.js
+++ index.js
@@ -1,5 +1,84 @@
-/* -----------------------------------RANDOM NUMBER GENERATOR
+/* -----------------------------------IF STATEMENTS
 */
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick = function (){
+
+    age = myText.value;
+    age = Number(age);
+
+    if(age >= 100){
+        resultElement.textContent = `You are too old to enter this site`;
+    }
+    else if(age == 0){
+        resultElement.textContent = `You can't enter. You were just born.`;
+    }
+    else if(age < 0){
+        resultElement.textContent = `Your age can't be below 0`;
+    }
+    else if(age >= 18){
+        resultElement.textContent = `You are old enough to enter this site`;
+    } 
+    else{
+        resultElement.textContent = `You must be 18+ to enter this site`;
+
+    }
+
+}
+
+/*
+let age = 25;
+let hasLicense = false;
+
+if(age >= 16){
+    console.log("You are old enough to drive.");
+
+    if(hasLicense){
+        console.log("You have your license!");
+    } else {
+        console.log("You do not have your license yet!");
+    }
+} else {
+    console.log("You must be 16+ to have a license");
+}
+*/
+/*
+let isStudent = false;
+
+if(isStudent){
+    console.log("You are a student!");
+}
+else {
+    console.log("You are NOT a student!");
+}
+*/
+/*
+let time = 14;
+
+if(time < 12){
+    console.log("Good morning!");
+}
+else
+{
+    console.log("Good afternoon!");
+}
+*/
+/*
+let age = 13;
+
+if(age >= 18){
+    console.log("You are old enought to enter this site");
+} else {
+    console.log("You must be 18+ to enter this site");
+}
+*/
+
+/* -----------------------------------RANDOM NUMBER GENERATOR
+
 
 /* Example:
 const min = 50;
@@ -9,6 +88,7 @@ let randomNum = Math.floor(Math.random() * (max - min)) + min;
 console.log(randomNum);
 */
 
+/*
 const myButton = document.getElementById("myButton");
 const label1 = document.getElementById("label1");
 const label2 = document.getElementById("label2");
@@ -27,6 +107,7 @@ myButton.onclick = function(){
     label2.textContent = randomNum2;
     label3.textContent = randomNum3;
 }
+*/
 
 /* -----------------------------------MATH OBJECT
 // Math: built-in object that provides a collection of properties and methods for doing math
