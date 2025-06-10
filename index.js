68354ce75d4b531dc557288237bc90399bd68786
FunctionJoey <functionjoey@gmail.com>
2025-06-10 14:12:13 -0600

    Checked property

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-/* -----------------------------------IF STATEMENTS
+/* -----------------------------------CHECKED PROPERTY
 */
+// .checked     = property that determines the checked state of an
+//                HTML checkbox of radio button element
+
+const myCheckBox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+
+    if(myCheckBox.checked){
+        subResult.textContent = `You are subscribed!`;
+    } else {
+        subResult.textContent = `You are not subscribed`;
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa`;
+    } else if(masterCardBtn.checked) {
+        paymentResult.textContent = `You are paying with Master Card`;
+    } else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal`;
+    }
+}
+
+
+/* -----------------------------------IF STATEMENTS
+
 
 const myText = document.getElementById("myText");
 const mySubmit = document.getElementById("mySubmit");
@@ -29,7 +60,7 @@ mySubmit.onclick = function (){
     }
 
 }
-
+*/
 /*
 let age = 25;
 let hasLicense = false;
