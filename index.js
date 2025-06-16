ee30e9e3f2ff70c16b53198af89044613e1869ec
FunctionJoey <functionjoey@gmail.com>
2025-06-16 12:35:02 -0600

    Method Chaining

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+/* -----------------------------------METHOD CHAINING
+*/
+
+let userName = window.prompt("Enter your username: ");
+
+// ------------------------- NO METHOD CHAINING
+
+/*
+userName = userName.trim();
+
+let letter = userName.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChars = userName.slice(1);
+extraChars = extraChars.toLowerCase();
+
+userName = letter + extraChars;
+console.log(userName);
+*/
+// ------------------------- METHOD CHAINING
+
+userName = userName.trim().charAt(0).toUpperCase() 
+            + userName.trim().slice(1).toLowerCase();
+
+console.log(userName);
+
 /* -----------------------------------STRING SLICING
 */
 
