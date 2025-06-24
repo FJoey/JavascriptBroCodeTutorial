ab78bfe6efd2d0466d24a9a4459984af914ee76e
FunctionJoey <functionjoey@gmail.com>
2025-06-24 13:06:40 -0600

    Strict Equality Operator

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+/* -----------------------------------STRICT EQUALITY
+*/
+/*
+const PI = 3.14;
+
+if(PI == "3.14"){
+    console.log("That is PI");
+} else{
+    console.log("That is NOT PI");
+}
+
+if(PI === "3.14"){
+    console.log("That is PI");
+} else{
+    console.log("That is NOT PI");
+}
+
+if(PI != "3.14"){
+    console.log("That is NOT PI");
+} else{
+    console.log("That is PI");
+}
+
+const PIstr = "3.14";
+
+if(PIstr !== "3.14"){
+    console.log("That is NOT PI");
+} else{
+    console.log("That is PI");
+}
+*/
+
 /* -----------------------------------LOGICAL OPERATORS
  */
 
