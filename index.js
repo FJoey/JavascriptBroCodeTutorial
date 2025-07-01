8a4a49a78462632e09c19d698438520e270fd8bc
FunctionJoey <functionjoey@gmail.com>
2025-06-30 18:55:54 -0600

    While Loops

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+/* -----------------------------------WHILE LOOPS
+*/
+
+let loggedIn = false;
+let username;
+let password;
+
+do {
+    username = window.prompt(`Enter your username`);
+    password = window.prompt(`Enter your password`);
+
+    if(username === "myUsername" && password === "myPassword"){
+        loggedIn = true;
+        console.log("You are logged in!");
+    } else {
+        console.log("Invalid credentials! Please try again");
+    }
+} while(!loggedIn);
+
+// let username = "";
+
+/*
+while(username === "" || username === null){
+    username = window.prompt(`Enter your name`);
+} 
+*/
+
+/*
+do{
+    username = window.prompt(`Enter your name`);
+} while(username === "" || username === null);
+
+console.log(`Hello ${username}`);
+*/
+
 /* -----------------------------------STRICT EQUALITY
 */
 /*
